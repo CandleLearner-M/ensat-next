4efae28c939ee99cb0f55747c7ab1ad314363a8f
Mostafa <candlelearner.m.dev@gmail.com>
2025-03-14 21:13:11 +0000

    Working on some animations

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,18 +1,81 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 import styles from "./Hero.module.scss";
 import HeroBackground from "./HeroBackground";
 
 function Hero() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
+
+  const lineVariants = {
+    initial: {
+      height: 0,
+      opacity: 0,
+    },
+    animate: {
+      opacity: [0, 1, 0.8, 1],
+      height: "200px",
+      transition: {
+        height: { duration: 1.8, ease: [0.19, 1, 0.22, 1] },
+        opacity: { duration: 2.2, ease: [0, 0.4, 0.7, 1] },
+      },
+    },
+  };
+
+
+  const containerVariants = {
+    animate: {
+      transition: {
+        staggerChildren: 0.3,
+        delayChildren: 0.2,
+      },
+    },
+  };
+
+  const textVariants = {
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 1,
+        ease: "easeOut",
+      },
+    },
+  };
+
   return (
     <main className={styles.hero}>
       <HeroBackground />
+      <motion.div
+        initial="initial"
+        animate={isVisible ? "animate" : "initial"}
+        variants={lineVariants}
+        className={styles.growing_line}
+      ></motion.div>
 
-      <h1>École Nationale des Sciences Appliquées de Tanger</h1>
-      <p>
-        L’ENSAT, depuis 1998 : une formation d’ingénieur d’excellence, tournée
-        vers l’innovation et l’industrie.
-      </p>
-
-      <div className={styles.hero__content}></div>
+      <motion.div
+        className={styles.hero_text_content}
+        initial="initial"
+        animate={isVisible ? "animate" : "initial"}
+        variants={containerVariants}
+      >
+        <motion.h1 variants={textVariants}>
+          École Nationale des Sciences Appliquées de Tanger
+        </motion.h1>
+        <motion.p variants={textVariants}>
+          L’ENSAT, depuis 1998 : une formation d’ingénieur d’excellence, tournée
+          vers l’innovation et l’industrie.
+        </motion.p>
+      </motion.div>
     </main>
   );
 }
