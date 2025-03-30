f9b31d015fb98f8706588c079b89c397e39ee63e
Mostafa <candlelearner.m.dev@gmail.com>
2025-03-30 11:24:44 +0000

    working on multilingual support

--- components/Navigation/NavBar/NavBar.tsx
+++ components/Navigation/NavBar/NavBar.tsx
@@ -1,23 +1,28 @@
-import Link from "next/link";
 import { FiSearch } from "react-icons/fi";
 import Logo from "../../Logo/Logo";
 import MenuBtn from "./MenuBtn";
 import styles from "./NavBar.module.scss";
+import { useTranslations } from "next-intl";
+import { Link } from "@/i18n/navigation";
 
 function NavBar() {
+  const t = useTranslations("Navigation.NavBar");
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar__container}>
         <div className={styles.navbar__brand}>
-          {/* Use Link normally without onClick handler */}
           <Link href="/" className={styles.navbar__logo}>
             <Logo />
           </Link>
         </div>
         <div className={styles.navbar__actions}>
-          <button className={styles.navbar__action} aria-label="Search">
+          <button
+            className={styles.navbar__action}
+            aria-label={t("search")}
+          >
             <FiSearch size={30} className={styles.navbar__icon} />
-            <span className={styles.navbar__actionText}>Search</span>
+            <span className={styles.navbar__actionText}>{t("search")}</span>
           </button>
 
           <MenuBtn />
@@ -26,4 +31,5 @@ function NavBar() {
     </nav>
   );
 }
+
 export default NavBar;
