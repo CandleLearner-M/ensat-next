a9b88ba3f956736b0225062331930e3185dcf7ec
Mostafa <candlelearner.m.dev@gmail.com>
2025-03-30 21:08:05 +0000

    working on bg

--- components/Hero/HeroBackground.tsx
+++ components/Hero/HeroBackground.tsx
@@ -1,18 +1,35 @@
+"use client";
+
 import Image from "next/image";
 import heroBg from "../../assets/hero-bg.jpg";
+
+import heroBgDesktop from "@/assets/hero-bg-mobile.jpg";
+
 import styles from "./HeroBackground.module.scss";
+import { useScreenSize } from "@/utils/useScreenSize";
 
 function HeroBackground() {
+  const { isDesktop } = useScreenSize();
+
   return (
     <div className={styles.hero__background}>
-      <Image
-        src={heroBg}
-        alt="ENSAT"
-        className={styles.hero__background__image}
-        fill
-        priority
-      
-      />
+      {isDesktop ? (
+        <Image
+          src={heroBgDesktop}
+          alt="ENSAT"
+          className={styles.hero__background__image}
+          fill
+          priority
+        />
+      ) : (
+        <Image
+          src={heroBg}
+          alt="ENSAT"
+          className={styles.hero__background__image}
+          fill
+          priority
+        />
+      )}
 
       <div className={styles.gradient_overlay}></div>
     </div>
